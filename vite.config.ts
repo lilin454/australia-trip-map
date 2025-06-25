23c6cedccdebc44f42a99343ab82990322258063
lilin454 <83146777+lilin454@users.noreply.github.com>
2025-06-25 16:55:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/australia-trip-map/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
